db1f8fbb60c8b9a69c9994d0d809b8c5c6774347
Kate <keiti93@users.noreply.github.com>
2015-04-26 00:32:06 +0300

    Increasing sequence

---
+++ week0/Problem-18.py
@@ -0,0 +1,11 @@
+def is_increasing(seq):
+    for index in range(0, len(seq)-1):
+        if seq[index]>=seq[index+1]:
+            return False
+    return True
+
+print (is_increasing([1,2,3,4,5]))
+print (is_increasing([1]))
+print (is_increasing([5,6,-10]))
+print (is_increasing([1,1,1,1]))
+        
